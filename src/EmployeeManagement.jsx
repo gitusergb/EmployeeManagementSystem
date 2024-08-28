22cceaba197683851422d70176fbf8fb0bed3d91
gitusergb <g4ur131@gmail.com>
2024-08-28 21:09:27 +0530

    ems

--- src/EmployeeManagement.jsx
+++ src/EmployeeManagement.jsx
@@ -69,6 +69,7 @@ const EmployeeManagement=()=> {
             <option value="">All Genders</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
+            <option value="Other">Other</option>
           </select>
           </div>
           <button onClick={() => handleSort('asc')}> Salary Asc ↑</button>
